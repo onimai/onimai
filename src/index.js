8e19b3ab3e311f602aa4187be0778800a3515ae3
Ling-yunchi <wr020210@gmail.com>
2023-02-04 23:25:57 +0800

    📝 update: add some test

--- src/index.js
+++ src/index.js
@@ -1,63 +1,93 @@
-const components = require("../dist/components")
-const mainFunction = ({ addBase, addComponents, addUtilities, config, postcss }) => {
-  addComponents({
-    ...components,
-  })
-}
+// const mainFunction = ({ addBase, addComponents, addUtilities, config, postcss }) => {
+//   addComponents([
+//     {
+//       ".btn": {
+//         display: "inline-block",
+//         padding: ".5rem 1rem",
+//         borderRadius: ".25rem",
+//         fontWeight: "600",
+//         textTransform: "uppercase",
+//         textDecoration: "none",
+//       },
+//     },
+//   ])
+// }
 
-module.exports = require("tailwindcss/plugin")(mainFunction, {
-  theme: {
-    extend: {
-      colors: {
-        brown: {
-          50: "#faf7f6",
-          100: "#f4f0ee",
-          200: "#e4d8d4",
-          300: "#d3c1ba",
-          400: "#b39387",
-          500: "#926453",
-          600: "#835a4b",
-          700: "#6e4b3e",
-          800: "#583c32",
-          900: "#483129",
-        },
-        yellow: {
-          50: "#fffffa",
-          100: "#fffef5",
-          200: "#fffde6",
-          300: "#fffcd6",
-          400: "#fffab8",
-          500: "#FFF899",
-          600: "#e6df8a",
-          700: "#bfba73",
-          800: "#99955c",
-          900: "#7d7a4b",
-        },
-        red: {
-          50: "#fef9f9",
-          100: "#fdf3f4",
-          200: "#fbe1e2",
-          300: "#f9ced1",
-          400: "#f4aaaf",
-          500: "#EF858C",
-          600: "#d7787e",
-          700: "#b36469",
-          800: "#8f5054",
-          900: "#754145",
-        },
-        blue: {
-          50: "#f7fbfd",
-          100: "#eff8fb",
-          200: "#d8edf6",
-          300: "#c0e2f0",
-          400: "#91cde4",
-          500: "#62b7d9",
-          600: "#58a5c3",
-          700: "#4a89a3",
-          800: "#3b6e82",
-          900: "#305a6a",
+module.exports = (options) => {
+  return ({ addBase, addComponents, addUtilities, config, postcss }) => {
+    addComponents([
+      {
+        ".btn": {
+          display: "inline-block",
+          padding: ".5rem 1rem",
+          borderRadius: ".25rem",
+          fontWeight: "600",
+          textTransform: "uppercase",
+          textDecoration: "none",
         },
       },
-    },
-  },
-})
+    ])
+    addUtilities({
+      ".test": {
+        display: "block",
+      },
+    })
+  }
+}
+
+// module.exports = require("tailwindcss/plugin")(mainFunction, {
+//   theme: {
+//     extend: {
+//       colors: {
+//         brown: {
+//           50: "#faf7f6",
+//           100: "#f4f0ee",
+//           200: "#e4d8d4",
+//           300: "#d3c1ba",
+//           400: "#b39387",
+//           500: "#926453",
+//           600: "#835a4b",
+//           700: "#6e4b3e",
+//           800: "#583c32",
+//           900: "#483129",
+//         },
+//         yellow: {
+//           50: "#fffffa",
+//           100: "#fffef5",
+//           200: "#fffde6",
+//           300: "#fffcd6",
+//           400: "#fffab8",
+//           500: "#FFF899",
+//           600: "#e6df8a",
+//           700: "#bfba73",
+//           800: "#99955c",
+//           900: "#7d7a4b",
+//         },
+//         red: {
+//           50: "#fef9f9",
+//           100: "#fdf3f4",
+//           200: "#fbe1e2",
+//           300: "#f9ced1",
+//           400: "#f4aaaf",
+//           500: "#EF858C",
+//           600: "#d7787e",
+//           700: "#b36469",
+//           800: "#8f5054",
+//           900: "#754145",
+//         },
+//         blue: {
+//           50: "#f7fbfd",
+//           100: "#eff8fb",
+//           200: "#d8edf6",
+//           300: "#c0e2f0",
+//           400: "#91cde4",
+//           500: "#62b7d9",
+//           600: "#58a5c3",
+//           700: "#4a89a3",
+//           800: "#3b6e82",
+//           900: "#305a6a",
+//         },
+//       },
+//     },
+//   },
+// })
