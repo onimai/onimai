79f7ab1c0fa3e997342c0217cdd715b4e34574c2
Ling-yunchi <wr020210@gmail.com>
2023-02-05 18:09:32 +0800

    ✨ feat: add doc website

---
+++ src/docs/src/routes/docs/components/index.tsx
@@ -0,0 +1,12 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <div>
+      <h2>Button</h2>
+      <p>
+        <button>Click me</button>
+      </p>
+    </div>
+  );
+});
