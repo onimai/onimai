0c12a7710dd14e14f23f7f3ad089814c7eb6d941
swwind <swwind233@gmail.com>
2023-02-05 23:23:56 +0800

    add button & write docs

--- packages/onimai-docs/src/routes/index.tsx
+++ packages/onimai-docs/src/routes/index.tsx
@@ -6,32 +6,15 @@ import { Modal } from "@onimai/qwik";
 export default component$(() => {
   return (
     <div class="p-4 flex gap-4">
-      <button class="btn">DEFAULT</button>
-      <button class="btn btn-danger">DANGER</button>
-      <button class="btn btn-outline">OUTLINE</button>
-      <button class="btn btn-subtle">SUBTLE</button>
-      <button class="btn btn-ghost">GHOST</button>
-      <button class="btn btn-link">LINK</button>
-
-      <Link class="btn" href="/docs/get-start">
+      <Link class="btn btn-link" href="/docs/get-start">
         Get Start
       </Link>
-
-      <label for="hello" class="btn">
-        Hello
-      </label>
-
-      <Modal id="hello">
-        <h2 class="text-2xl font-bold">Settings</h2>
-        <p class="opacity-60">Are you sure to remove all your settings?</p>
-
-        <button class="btn btn-danger" q:slot="action">
-          Yes
-        </button>
-        <label class="btn" for="hello" q:slot="action">
-          No
-        </label>
-      </Modal>
+      <Link class="btn btn-link" href="/docs/button">
+        Buttons
+      </Link>
+      <Link class="btn btn-link" href="/docs/modal">
+        Modals
+      </Link>
     </div>
   );
 });
