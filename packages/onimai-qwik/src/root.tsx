61d655898a1e56b8fb5676ba55d818eeb3152cdd
swwind <swwind233@gmail.com>
2023-02-06 15:02:35 +0800

    feat: add input

--- packages/onimai-qwik/src/root.tsx
+++ packages/onimai-qwik/src/root.tsx
@@ -43,6 +43,28 @@ export default component$(() => {
           <h2 class="text-2xl font-bold">Hello world</h2>
           <p class="opacity-60">Include stdio.h</p>
         </Drawer>
+
+        <div class="max-w-sm">
+          <input type="text" class="input" placeholder="Text..."></input>
+          <input type="email" class="input" placeholder="Email..."></input>
+          <input type="color" class="input" placeholder="Color..."></input>
+          <input type="date" class="input" placeholder="Date..."></input>
+          <input
+            type="datetime-local"
+            class="input"
+            placeholder="Datetime Local..."
+          ></input>
+          <input type="file" class="input" placeholder="File..."></input>
+          <input type="image" class="input" placeholder="Image..."></input>
+          <input type="month" class="input" placeholder="Month..."></input>
+          <input type="number" class="input" placeholder="Number..."></input>
+          <input
+            type="password"
+            class="input"
+            placeholder="Password..."
+          ></input>
+          <input type="search" class="input" placeholder="Search..."></input>
+        </div>
       </body>
     </>
   );
