4d81e5153b0a381bf5ad0eab56df9a447a1db993
swwind <swwind233@gmail.com>
2023-02-06 16:48:39 +0800

    feat: add popover component

---
+++ packages/onimai-qwik/src/components/popover/popover.tsx
@@ -0,0 +1,53 @@
+import { component$, Slot } from "@builder.io/qwik";
+
+type Props = {
+  trigger?: "focus" | "hover";
+  direction?:
+    | "left"
+    | "left-top"
+    | "left-bottom"
+    | "right"
+    | "right-top"
+    | "right-bottom"
+    | "top"
+    | "top-left"
+    | "top-right"
+    | "bottom"
+    | "bottom-left"
+    | "bottom-right";
+  class?: string;
+};
+
+export const triggerClass = {
+  focus: "popover-focus",
+  hover: "popover-hover",
+};
+
+export const directionClass = {
+  left: "popover-left",
+  "left-top": "popover-left-top",
+  "left-bottom": "popover-left-bottom",
+  right: "popover-right",
+  "right-top": "popover-right-top",
+  "right-bottom": "popover-right-bottom",
+  top: "popover-top",
+  "top-left": "popover-top-left",
+  "top-right": "popover-top-right",
+  bottom: "popover-bottom",
+  "bottom-left": "popover-bottom-left",
+  "bottom-right": "popover-bottom-right",
+};
+
+export const Popover = component$((props: Props) => {
+  const trigger = triggerClass[props.trigger || "focus"];
+  const direction = directionClass[props.direction || "bottom-right"];
+
+  return (
+    <div class={`popover ${trigger}`}>
+      <Slot name="trigger" />
+      <div class={`popover-content ${direction} ${props.class}`}>
+        <Slot />
+      </div>
+    </div>
+  );
+});
