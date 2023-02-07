a67b5b8f23ef34bee1c180c1949072320b3c0f7c
Ling-yunchi <wr020210@gmail.com>
2023-02-07 23:44:21 +0800

    ✨ feat(onimai): add some color

--- packages/onimai/index.js
+++ packages/onimai/index.js
@@ -1,4 +1,16 @@
-module.exports = require("tailwindcss/plugin")(({ addComponents, addBase }) => {
-  addBase(require("./dist/base.js"));
-  addComponents(require("./dist/components.js"));
-});
+const colors = require("./src/colors");
+module.exports = require("tailwindcss/plugin")(
+  ({ addComponents, addBase }) => {
+    addBase(require("./dist/base.js"));
+    addComponents(require("./dist/components.js"));
+  },
+  {
+    theme: {
+      extend: {
+        colors: {
+          ...colors,
+        },
+      },
+    },
+  }
+);
