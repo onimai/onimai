307cc8057c990a411412dc312db26ee44b7229f8
Ling-yunchi <wr020210@gmail.com>
2023-02-08 18:39:06 +0800

    ✨ feat(onimai-docs): improve index page

--- packages/onimai-docs/src/routes/index.tsx
+++ packages/onimai-docs/src/routes/index.tsx
@@ -1,30 +1,80 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStyles$ } from "@builder.io/qwik";
 import { type DocumentHead, Link } from "@builder.io/qwik-city";
+import GithubIcon from "~/components/icons/GithubIcon";
 
 export default component$(() => {
+  useStyles$(`
+  @keyframes bg-scroll {
+    0% {
+      background-position: 0 0;
+    }
+    100% {
+      background-position: 1600px 560px;
+    }
+  }`);
   return (
-    <>
-      <div class="p-4 flex gap-4">
-        <Link class="btn btn-link" href="/docs/get-start">
-          Get Start
-        </Link>
-        <Link class="btn btn-link" href="/docs/button">
-          Buttons
-        </Link>
-        <Link class="btn btn-link" href="/docs/modal">
-          Modals
-        </Link>
+    <div class="bg-scroll bg-repeat [background-image:_url(https://onimai.jp/assets/img/common/mainbg.jpg)] [background-size:_800px_560px] [animation:_bg-scroll_linear_60s_infinite]">
+      <div
+        class="sticky top-0 z-10 flex h-20 justify-between p-4"
+        window:onScroll$={(_, el) => {
+          if (window.scrollY > 0) {
+            el.classList.add(...["bg-white", "shadow-lg", "bg-opacity-90"]);
+          } else {
+            el.classList.remove(...["bg-white", "shadow-lg", "bg-opacity-90"]);
+          }
+        }}
+      >
+        <img class="ml-4" src="https://onimai.jp/assets/img/common/logo.png" />
+        <div class="mr-4 flex items-center space-x-4">
+          <Link class="btn-outline btn" href="/docs/get-start">
+            Get Start
+          </Link>
+          <Link class="btn-outline btn" href="/docs/drawer">
+            Drawer
+          </Link>
+          <Link
+            class="btn-outline btn"
+            href="https://github.com/Ling-yunchi/onimaiui"
+          >
+            <GithubIcon />
+          </Link>
+        </div>
       </div>
-      <div class="flex justify-center [background-image:_url(https://onimai.jp/assets/img/common/mainbg.jpg)] bg-repeat [background-size:500px] bg-scroll">
-        <div class="flex flex-col max-w-[60rem] bg-white px-8">
-          <img src="https://onimai.jp/assets/img/common/logo.png" />
-          <div class="prose">
-            <div class="flex justify-center my-8">
-              <h1 class="text-yellow-800">
-                Onimai <font class="text-blue-400">U</font>
-                <font class="text-red-400">I</font>
-              </h1>
+      <div class="-mt-20 flex flex-col items-center">
+        <div class="flex w-full flex-col items-center bg-[rgba(0,0,0,0.4)] bg-[url(https://onimai.jp/episode/img/01_05.jpg)] bg-cover bg-center bg-blend-darken">
+          <div class="mt-20 flex w-full max-w-4xl flex-wrap justify-between">
+            <div class="p-4 lg:pl-10 lg:pb-20">
+              <div class="my-[6rem] text-center lg:text-left">
+                <h1 class="font-mono text-[4rem] font-bold text-o-brown-400">
+                  Onimai&nbsp;
+                  <font class="text-o-blue-400">U</font>
+                  <font class="text-o-pink-500">I</font>
+                </h1>
+                <h2 class="text-2xl font-bold text-white">纯CSS的萌系UI框架</h2>
+              </div>
+              <h2 class="mb-4 text-xl font-bold text-white">
+                我们只干三件事：
+              </h2>
+              <div class="flex flex-wrap space-x-8">
+                <div class="text-xl text-white">
+                  <ul class="space-y-2">
+                    <li>○ 可爱~</li>
+                    <li>○ 可爱~~</li>
+                    <li>○ 还是他妈的可爱！</li>
+                  </ul>
+                </div>
+                <code class="h-fit rounded-xl bg-black bg-opacity-20 p-4 text-white">
+                  npm i onimai
+                  <br />
+                  npm i @onimai/qwik
+                </code>
+              </div>
             </div>
+          </div>
+        </div>
+        <div class="flex max-w-4xl flex-col px-8">
+          <div class="prose">
+            <div class="my-8 flex justify-center"></div>
             <p>
               Onimai UI is a Cute CSS framework for building cartoon style UI
               for your Website.
@@ -49,7 +99,7 @@ export default component$(() => {
               />
             </p>
             <p>
-              <Link class="btn btn-outline" href="/docs/get-start">
+              <Link class="btn-outline btn" href="/docs/get-start">
                 Get Start
               </Link>
             </p>
@@ -58,11 +108,10 @@ export default component$(() => {
               <Link href="https://onimai.jp/">https://onimai.jp/</Link>
             </p>
             <br />
-            <br />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 });
 
