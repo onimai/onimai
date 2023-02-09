99bbbc375608870f863364a1b0b9a51cf46a9e3c
swwind <i@sww.moe>
2023-02-09 20:10:11 +0800

    move docs to root

--- packages/onimai-docs/src/entry.ssr.tsx
+++ packages/onimai-docs/src/entry.ssr.tsx
@@ -21,7 +21,6 @@ export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
     manifest,
     ...opts,
-    base: "/onimaiui/",
     // Use container attributes to set attributes on the html tag.
     containerAttributes: {
       lang: "en-us",
--- packages/onimai-docs/vite.config.ts
+++ packages/onimai-docs/vite.config.ts
@@ -5,11 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite({
-      // client:{
-      //   outDir: 'docs',
-      // }
-    }), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
